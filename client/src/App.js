5c4f172b58fd9715de5c2685bae1b5c26234aa26
Mike Heavers <mheavers@gmail.com>
2022-12-14 14:15:26 -0800

    add summarizer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.scss";
+
+const getTagArray = function (str) {
+  const doc = new DOMParser().parseFromString(str, "text/html");
+  const arr = [...doc.body.childNodes].map((child) => {
+    const headlineItem = {
+      content: child.textContent,
+      type: child.nodeName,
+    };
+    return headlineItem;
+  });
+  return arr;
+};
+
+function querySelectorIncludesText(selector, text) {
+  return Array.from(document.querySelectorAll(selector)).find((el) =>
+    el.textContent.includes(text)
+  );
+}
+
+function handleLinkClick(e) {
+  const content = e.target.dataset.content;
+  const selector = e.target.dataset.selector.toLowerCase();
+
+  const $match = querySelectorIncludesText(selector, content);
+  console.log($match);
+  $match.scrollIntoView({ behavior: "smooth" });
+}
 
 function App() {
   const [data, setData] = React.useState(null);
   const [inputVal, setInputVal] = useState("");
   const [results, setResults] = React.useState(null);
+  const [headlineArray, setHeadlineArray] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   React.useEffect(() => {
     fetch("/api")
@@ -12,16 +41,26 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  useEffect(() => {
+    if (results?.articleHeadlines) {
+      const headlineArray = getTagArray(results.articleHeadlines);
+      console.log(headlineArray);
+      setHeadlineArray(headlineArray);
+    }
+  }, [results]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     fetch(`/parseURL?url=${inputVal}`)
       .then((res) => res.json())
       .then((data) => {
+        setIsLoading(false);
         if (data.error) {
           setData(data.error);
           console.log(data.details);
         } else {
-          console.log(data);
+          // console.log(data);
           setData("Displaying Results");
           setResults(data);
         }
@@ -30,43 +69,78 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Pocket Summary and Outline Creator</h1>
       <header className="App-header">
         <p>{!data ? "Loading..." : data}</p>
       </header>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="inputVal"
-          placeholder="Enter URL to parse"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-        />
-        <button type="submit" className="analyze-submit analyze-el">
-          Parse
-        </button>
-      </form>
-      {results && (
-        <div className="results">
-          <ul>
-            {Object.entries(results).map(([k, v]) => {
-              if (k === "topImageUrl" || k === "logo") {
-                return (
-                  <li>
-                    <span className="parsedKey">{k}</span>: <br />
-                    <img className="parsedImage" src={v} />
-                  </li>
-                );
-              } else {
-                return (
-                  <li key={`data-${k}`}>
-                    <span className="parsedKey">{k}</span>: {v}
-                  </li>
-                );
-              }
-            })}
-          </ul>
+      <main>
+        <div className="App-form">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="inputVal">Enter URL to parse</label>
+            <br />
+            <input
+              type="text"
+              className="input"
+              id="inputVal"
+              placeholder="Enter URL to parse"
+              value={inputVal}
+              onChange={(e) => setInputVal(e.target.value)}
+            />
+            <button type="submit" className="analyze-submit analyze-el">
+              Parse
+            </button>
+          </form>
         </div>
-      )}
+        {results ? (
+          <div className="App-results">
+            <div className="results-container">
+              <div className="results-sidebar">
+                <div className="outline-container">
+                  <h3>Skip to</h3>
+                  <ul>
+                    {headlineArray &&
+                      headlineArray.map((item, i) => {
+                        return (
+                          <li key={`outline-${i}`}>
+                            <a
+                              className="link-outline"
+                              onClick={handleLinkClick}
+                              data-content={item.content}
+                              data-selector={item.type}
+                            >
+                              {item.content}
+                            </a>{" "}
+                            ({item.type})
+                          </li>
+                        );
+                      })}
+                  </ul>
+                </div>
+                {results.articleSummaries &&
+                  results.articleSummaries.length > 0 && (
+                    <div className="summary-container">
+                      <h3>Summary</h3>
+                      <ul>
+                        {results.articleSummaries.map((val, i) => {
+                          return <li key={`outline-${i}`}>{val}</li>;
+                        })}
+                      </ul>
+                    </div>
+                  )}
+              </div>
+              <div className="results-main">
+                <h2>{results.title}</h2>
+                <div
+                  className="results-article"
+                  dangerouslySetInnerHTML={{ __html: results.article }}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>{isLoading && "Loading"}</div>
+        )}
+      </main>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,93 @@ const path = require("path");
 const express = require("express");
 let dotenv = require("dotenv").config();
 const axios = require("axios");
+const { convert } = require("html-to-text");
 
+const cheerio = require("cheerio");
+
+const NUM_PARAGRAPHS_PER_SUMMARY = 5;
 const PORT = process.env.PORT || 3001;
+const NLP_API_KEY = process.env.NLP_API_KEY;
+
+let numGroupsSummarized = 0;
 
 const app = express();
+
+const NLPCloudClient = require("nlpcloud");
+const client = new NLPCloudClient("bart-large-cnn", NLP_API_KEY);
+
+const buildParagraphs = function (str) {
+  const doc = cheerio.load(str, null, false);
+
+  let arr = [];
+  doc._root.children.forEach((child, i) => {
+    let childStr = `${child.children[0].data}`.replace(/(\r\n|\n|\r)/gm, " ");
+    childStr.trim();
+    arr.push(childStr);
+  });
+  return arr;
+};
+
+const summarize = async function (text) {
+  const summaryPromise = new Promise((resolve, reject) => {
+    client
+      .summarization(`${text}`)
+      .then(async function (response) {
+        //console.log("async summary response:");
+        //console.log(response.data);
+        resolve(response.data?.summary_text || "");
+      })
+      .catch(function (err) {
+        console.error(err.response.status);
+        console.error(err.response.data);
+        resolve("error");
+      });
+  });
+  return summaryPromise;
+};
+
+const summarizeParagraphs = async function (parsedParagraphs) {
+  const paragraphSummaries = [];
+  const numGroupsToSummarize = Math.floor(
+    parsedParagraphs.length / NUM_PARAGRAPHS_PER_SUMMARY
+  );
+
+  const paragraphGroupings = [];
+
+  for (let i = 0; i < numGroupsToSummarize; i++) {
+    const textToSummarize = parsedParagraphs
+      .slice(
+        i * NUM_PARAGRAPHS_PER_SUMMARY,
+        (i + 1) * NUM_PARAGRAPHS_PER_SUMMARY
+      )
+      .join(" ");
+    paragraphGroupings.push(textToSummarize);
+  }
+
+  const summarizeAllPromise = new Promise((resolve, reject) => {
+    paragraphGroupings.forEach(async (paragraphGrouping) => {
+      const summary = await summarize(paragraphGrouping);
+      numGroupsSummarized++;
+      console.log(
+        "num groups summarized",
+        numGroupsSummarized,
+        numGroupsToSummarize
+      );
+      console.log("summarized (should not come before async summary response)");
+      console.log(summary);
+      if (summary !== "error") {
+        paragraphSummaries.push(summary);
+      }
+
+      if (numGroupsSummarized === numGroupsToSummarize) {
+        console.log("finished summarizing");
+        console.log(paragraphSummaries);
+        resolve(paragraphSummaries);
+      }
+    });
+  });
+  return summarizeAllPromise;
+};
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
@@ -17,12 +100,7 @@ app.get("/api", (req, res) => {
 
 app.get("/parseURL", (req, res) => {
   const data = req.query.url;
-  console.log("data", data);
   const requestURL = `${process.env.API_URL}?consumer_key=${process.env.KEY}&url=${data}`;
-  console.log("requestURL", requestURL);
-  // return res.json({
-  //   data: requestURL,
-  // });
 
   axios
     .get(requestURL, {
@@ -30,18 +108,72 @@ app.get("/parseURL", (req, res) => {
         Accept: "application/json",
       },
     })
-    .then((response) => {
-      const { host, title, excerpt, topImageUrl, domainMetadata } =
-        response.data;
+    .then(async (response) => {
+      //destructure response into items you want to use
+      const { title, article } = response.data;
+
+      const articleHeadlines = convert(article, {
+        baseElements: {
+          selectors: ["h1", "h2", "h3", "h4", "h5", "h6"],
+        },
+        selectors: [
+          {
+            selector: "h1",
+            format: "inlineTag",
+          },
+          {
+            selector: "h2",
+            format: "inlineTag",
+          },
+          {
+            selector: "h3",
+            format: "inlineTag",
+          },
+          {
+            selector: "h4",
+            format: "inlineTag",
+          },
+          {
+            selector: "h5",
+            format: "inlineTag",
+          },
+          {
+            selector: "h6",
+            format: "inlineTag",
+          },
+        ],
+      });
+
+      const articleParagraphs = convert(article, {
+        baseElements: {
+          selectors: ["p"],
+        },
+        selectors: [
+          {
+            selector: "p",
+            format: "inlineTag",
+            options: {
+              ignoreHref: true,
+              ignoreImage: true,
+              noAnchorUrl: true,
+              noLinkBrackets: true,
+              limits: {
+                maxChildNodes: 5,
+              },
+            },
+          },
+        ],
+      });
+
+      const parsedParagraphs = buildParagraphs(articleParagraphs);
+      const summarizedParagraphs = await summarizeParagraphs(parsedParagraphs);
       const apiResponseObj = {
-        host: host,
         title: title,
-        excerpt: excerpt,
-        topImageUrl: topImageUrl,
-        logo: domainMetadata.logo,
+        article: article,
+        articleHeadlines: articleHeadlines,
+        articleSummaries: summarizedParagraphs,
       };
-      //return any relevant data here
-      console.log(apiResponseObj);
+
       res.json(apiResponseObj);
     })
     .catch((err) => {
